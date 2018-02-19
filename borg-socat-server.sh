4056f251648ff9a55f74d77168ec04f55f779bcf
Alexandre L <alex131089@msn.com>
2018-02-20 00:58:38 +0100

    Update server wrapper (fix kill, cd to restricted)

--- borg-socat-server.sh
+++ borg-socat-server.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+#BORGW_PATH=/srv/borg
+#BORGW_PATH=/opt/borg
+BORGW_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 if [ $# -lt 3 ]; then
   echo "usage: [-s|--socket] local-listening-uds.sock remote-connecting-uds.sock target-wrapper host <borg command>"
   echo "usage: [-t|--tcp] local-listening-port remote-connecting-port target-wrapper host <borg command>"
-  echo "example: BORGW_RESTRICT_PATH=/path/to/repo $0 -s /tmp/local.sock /tmp/remote.sock /root/borg-socat-client.sh"\
-    "backuped-server /root/borg create ssh://server/path/to/repo::{hostname}_{utcnow} path to backup"
+
+  echo "example: BORGW_RESTRICT_PATH=/path/to/repos $0 -s /tmp/local.sock /tmp/remote.sock /root/borg-socat-client.sh"\
+    "\"backuped-server -p 22\" /root/borg create ssh://backup-server/./my-repo::{hostname}_{utcnow} paths to backup"
+  echo "example: SSH_ARGS=\"-o ProxyCommand=ssh -W %h:%p gateway-server -p 22\" BORGW_RESTRICT_REPOSITORY=/path/to/repos/repo"\
+    "$0 -s /tmp/local.sock /tmp/remote.sock /root/borg-socat-client.sh backuped-server /root/borg"\
+    "create ssh://backup-server/./::{hostname}_{utcnow} paths to backup"
+
 # Serve parameters
 #BORGW_RESTRICT_PATH
 #BORGW_RESTRICT_REPOSITORY
@@ -19,27 +28,33 @@ else
     python -c "import sys; print(sys.argv[1:])" "$@"
   }
 
-  borg_serve_cmd="./borg serve --umask 077"
+  borg_serve_cmd=$BORGW_PATH"/borg serve --umask 077"
   [ "$BORGW_APPEND_ONLY" == '1' -o "$BORGW_APPEND_ONLY" == 'y' -o "$BORGW_APPEND_ONLY" == 'yes' ] && borg_serve_cmd="$borg_serve_cmd --append-only"
-  [ -n "$BORGW_RESTRICT_PATH" ] && borg_serve_cmd="$borg_serve_cmd --restrict-to-path $BORGW_RESTRICT_PATH"
-  [ -n "$BORGW_RESTRICT_REPOSITORY" ] && borg_serve_cmd="$borg_serve_cmd --restrict-to-repository $BORGW_RESTRICT_REPOSITORY"
   [ -n "$BORGW_STORAGE_QUOTA" ] && borg_serve_cmd="$borg_serve_cmd --storage-quota $BORGW_STORAGE_QUOTA"
+  if [ -n "$BORGW_RESTRICT_REPOSITORY" ]; then
+    borg_serve_cmd="$borg_serve_cmd --restrict-to-repository $BORGW_RESTRICT_REPOSITORY"
+    cd $BORGW_RESTRICT_REPOSITORY
+  elif [ -n "$BORGW_RESTRICT_PATH" ]; then
+    borg_serve_cmd="$borg_serve_cmd --restrict-to-path $BORGW_RESTRICT_PATH"
+    cd $BORGW_RESTRICT_PATH
+  fi
 
+  [ -z "$SSH_ARGS" ] && SSH_ARGS=--
   if [ "$1" == '-s' -o "$1" == '--socket' ]; then
     print_args socat UNIX-LISTEN:"$2" "EXEC:$borg_serve_cmd"
     [ "$BORGW_DRYRUN" == "1" ] || socat UNIX-LISTEN:"$2" "EXEC:$borg_serve_cmd" &
     socat_pid=$!
-    print_args ssh -R "$2":"$3" "$SSH_ARGS" $5 "BORG_RSH=\"$4 -s $3\"" "${@:6}"
-    [ "$BORGW_DRYRUN" == "1" ] || ssh -R "$2":"$3" "$SSH_ARGS" $5 "BORG_RSH=\"$4 -s $3\"" "${@:6}"
-    [ "$BORGW_DRYRUN" == "1" ] || kill $socat_pid
+    print_args ssh -R "$2":"$3" $5 "$SSH_ARGS" "BORG_RSH=\"$4 -s $3\"" "${@:6}"
+    [ "$BORGW_DRYRUN" == "1" ] || ssh -R "$2":"$3" $5 "$SSH_ARGS" "BORG_RSH=\"$4 -s $3\"" "${@:6}"
+    [ "$BORGW_DRYRUN" == "1" ] || (kill -0 "$socat_pid" && kill $socat_pid)
   elif [ "$1" == '-t' -o "$1" == '--tcp' ]; then
     host=localhost
     print_args socat TCP-LISTEN:"$2" "EXEC:$borg_serve_cmd"
     [ "$BORGW_DRYRUN" == "1" ] || socat TCP-LISTEN:"$2" "EXEC:$borg_serve_cmd" &
     socat_pid=$!
-    print_args ssh -R "$2":"$host":"$3" "$SSH_ARGS" $5 "BORG_RSH=\"$4 -t $3\"" "${@:6}"
-    [ "$BORGW_DRYRUN" == "1" ] || ssh -R "$2":"$host":"$3" "$SSH_ARGS" $5 "BORG_RSH=\"$4 -t $3\"" "${@:6}"
-    [ "$BORGW_DRYRUN" == "1" ] || kill $socat_pid
+    print_args ssh -R "$2":"$host":"$3" $5 "$SSH_ARGS" "BORG_RSH=\"$4 -t $3\"" "${@:6}"
+    [ "$BORGW_DRYRUN" == "1" ] || ssh -R "$2":"$host":"$3" $5 "$SSH_ARGS" "BORG_RSH=\"$4 -t $3\"" "${@:6}"
+    [ "$BORGW_DRYRUN" == "1" ] || (kill -0 "$socat_pid" && kill $socat_pid)
   else
     echo "unknown method"
   fi
